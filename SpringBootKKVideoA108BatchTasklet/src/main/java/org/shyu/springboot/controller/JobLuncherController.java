aaabba676a68668c0d06eeed3d45c208d937c4d3
jackyshyu <jackyshyu@gmail.com>
2018-10-19 13:38:35 -0500

    Finished

--- SpringBootKKVideoA108BatchTasklet/src/main/java/org/shyu/springboot/controller/JobLuncherController.java
+++ SpringBootKKVideoA108BatchTasklet/src/main/java/org/shyu/springboot/controller/JobLuncherController.java
@@ -1,5 +1,29 @@
 package org.shyu.springboot.controller;
 
-public class JobLuncherController {
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class JobLuncherController {
+	@Autowired
+	private JobLauncher jobLauncher;
+	
+	@Autowired
+	private Job job;
+	
+	@RequestMapping(name="/launchjob")
+	public String handle() {
+		try {
+			JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters();
+			jobLauncher.run(job, jobParameters);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return "Done!";
+	}
 }
--- SpringBootKKVideoA108BatchTasklet/src/main/java/org/shyu/springboot/tasklet/TaskletStep.java
+++ SpringBootKKVideoA108BatchTasklet/src/main/java/org/shyu/springboot/tasklet/TaskletStep.java
@@ -6,13 +6,15 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TaskletStep implements Tasklet{
 
 	@Override
 	public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
 		try {
-			File file = new File("MyFile/test.txt");
+			File file = new File("MyFiles/test.txt");
 			if (file.delete()) {
 				System.out.println("TaskletStep: " + file.getName() + " is deleted!");
 			} else {
